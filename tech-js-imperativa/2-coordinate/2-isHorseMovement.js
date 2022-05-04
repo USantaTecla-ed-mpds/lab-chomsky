4a45f385ed089a650731bfea790791d6b2c8abde
Miguel Angel Bernabe Cruz <ma.bernabecruz@gmail.com>
2022-05-04 13:39:24 +0200

    Improve isHorseMovement solution

--- tech-js-imperativa/2-coordinate/2-isHorseMovement.js
+++ tech-js-imperativa/2-coordinate/2-isHorseMovement.js
@@ -1,8 +1,8 @@
 const { Console } = require("console-mpds");
 const console = new Console(); 
 
-const HORSE_MOVE_AXIS_LONG_INCREMENT = 2; 
-const HORSE_MOVE_AXIS_SHORT_INCREMENT = 1;
+const HORSE_MOVE_LONG = 2; 
+const HORSE_MOVE_SHORT = 1;
 
 console.writeln(`Coordenada origen:`)
 const abscissaOrigin = console.readNumber(`Dame la abcisa de la coordenada: `);
@@ -10,15 +10,10 @@ const ordinateOrigin  = console.readNumber(`Dame la ordenada de la coordenada: `
 console.writeln(`Coordenada destino:`)
 const abscissaTarget = console.readNumber(`Dame la abcisa de la coordenada: `);
 const ordinateTarget  = console.readNumber(`Dame la ordenada de la coordenada: `);
-let abscissaAbsIncrement = abscissaTarget - abscissaOrigin;
-abscissaAbsIncrement = (abscissaAbsIncrement < 0) ? -abscissaAbsIncrement : abscissaAbsIncrement;
-let ordinateAbsIncrement = ordinateTarget - ordinateOrigin;
-ordinateAbsIncrement = (ordinateAbsIncrement < 0) ? -ordinateAbsIncrement : ordinateAbsIncrement;
-const abcissaIsLongIncrement = abscissaAbsIncrement == HORSE_MOVE_AXIS_LONG_INCREMENT;
-const abcissaIsShortIncrement = abscissaAbsIncrement == HORSE_MOVE_AXIS_SHORT_INCREMENT;
-const ordinateIsLongIncrement = ordinateAbsIncrement == HORSE_MOVE_AXIS_LONG_INCREMENT;
-const ordinateIsShortIncrement = ordinateAbsIncrement == HORSE_MOVE_AXIS_SHORT_INCREMENT;
-let isHorseMove = (abcissaIsLongIncrement && ordinateIsShortIncrement || abcissaIsShortIncrement && ordinateIsLongIncrement);
+const abscissaAbsIncrement = (abscissaTarget - abscissaOrigin < 0 ? -1 : 1) * (abscissaTarget - abscissaOrigin);
+const ordinateAbsIncrement = (ordinateTarget - ordinateOrigin < 0 ? -1 : 1) * (ordinateTarget - ordinateOrigin);
+let isHorseMove = (abscissaAbsIncrement == HORSE_MOVE_LONG && ordinateAbsIncrement == HORSE_MOVE_SHORT 
+    || abscissaAbsIncrement == HORSE_MOVE_SHORT && ordinateAbsIncrement == HORSE_MOVE_LONG);
 console.writeln(`La coordenada (${abscissaOrigin},${ordinateOrigin}) \
 y la coordenada destino (${abscissaTarget},${ordinateTarget}) \
 ${(isHorseMove)? "si":"no"} es un movimiento de caballo`); 
