693cf8619b031cb0b4af991412a867b327aef9e1
Miguel Angel Bernabe Cruz <ma.bernabecruz@gmail.com>
2022-05-06 04:22:11 +0200

    Add seasons solution

--- tech-js-estructurada/1-seasonWithPart/v0.js
+++ tech-js-estructurada/1-seasonWithPart/v0.js
@@ -1,51 +1,51 @@
 const { Console } = require("console-mpds");
 const console = new Console();
 
-const SPRING_BEGINNING_DAY = 21
-const DAYS_IN_MONTH = 30;
+DAYS_IN_MONTH = 30;
 const DAYS_IN_SEASON = DAYS_IN_MONTH * 3;
-const DAYS_FROM_WINTER_TO_YEAR_END = DAYS_IN_SEASON - SPRING_BEGINNING_DAY;
+const SPRING_BEGINNING_DAY = 21 + 2 * DAYS_IN_MONTH;
+const SUMMER_BEGINNING_DAY = SPRING_BEGINNING_DAY + DAYS_IN_SEASON;
+const AUTUMN_BEGINNING_DAY = SUMMER_BEGINNING_DAY + DAYS_IN_SEASON;
+const WINTER_BEGINNING_DAY = AUTUMN_BEGINNING_DAY + DAYS_IN_SEASON;
+const DAYS_WINTER_TO_END_YEAR = 365 - WINTER_BEGINNING_DAY;
+
 const DAYS_IN_SEASON_INTERVAL = 30;
+
 
 const day = console.readNumber(`Escriba un día (1-${DAYS_IN_MONTH}): `)
 const month = console.readNumber(`Escriba un mes (1-12): `)
 const year = console.readNumber(`Escriba un año (1-...): `)
 
 const dayOfTheYear = (month - 1) * DAYS_IN_MONTH + day;
-const daysFromWinter = dayOfTheYear + DAYS_FROM_WINTER_TO_YEAR_END;
-let season;
-
-switch (~~(daysFromWinter / DAYS_IN_SEASON)) {
-    case 0:
-        season = 'invierno';
-        break;
-    case 1:
-        season = 'primavera';
-        break;
-    case 2:
-        season = 'verano';
-        break;
-    case 3:
-        season = 'otoño';
-        break;
-    case 4:
-        season = 'invierno';
-        break;
+let season = 'invierno';
+let daysFromStationBegginning = day + DAYS_WINTER_TO_END_YEAR;
+if (SPRING_BEGINNING_DAY <= dayOfTheYear && dayOfTheYear < SUMMER_BEGINNING_DAY) {
+    season = 'primavera';
+    daysFromStationBegginning = dayOfTheYear - SPRING_BEGINNING_DAY;
+}
+if (SUMMER_BEGINNING_DAY <= dayOfTheYear && dayOfTheYear < AUTUMN_BEGINNING_DAY) {
+    season = 'verano';
+    daysFromStationBegginning = dayOfTheYear - SUMMER_BEGINNING_DAY;
+}
+else if (AUTUMN_BEGINNING_DAY <= dayOfTheYear && dayOfTheYear < WINTER_BEGINNING_DAY) {
+    season = 'otoño';
+    daysFromStationBegginning = dayOfTheYear - AUTUMN_BEGINNING_DAY;
+}
+else if (WINTER_BEGINNING_DAY <= dayOfTheYear) {
+    daysFromStationBegginning = dayOfTheYear - WINTER_BEGINNING_DAY;
 }
 
-let seasonInterval;
-let daysFromStationBegginning = daysFromWinter % DAYS_IN_SEASON;
-switch (~~(daysFromStationBegginning / DAYS_IN_SEASON_INTERVAL)) {
-    case 0:
-        seasonInterval = 'primeros';
-        break;
-    case 1:
-        seasonInterval = 'mediados';
-        break;
-    case 2:
-        seasonInterval = 'finales';
-        break;
+let seasonInterval = 'finales';
+if (0 <= daysFromStationBegginning && daysFromStationBegginning < 30) {
+    seasonInterval = 'principios';
 }
+else if (30 <= daysFromStationBegginning && daysFromStationBegginning < 60) {
+    seasonInterval = 'mediados';
+}
+
+console.writeln(`El día ${dayOfTheYear} desde inicio ${daysFromStationBegginning} `);
+
+
 
 console.writeln(`El día ${day} del ${month} de ${year} cae a ${seasonInterval} de ${season}.`);
 
