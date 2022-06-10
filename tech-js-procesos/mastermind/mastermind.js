ca95bf6fcbb32e491c0ad70889d107ae7ac8500c
Miguel Angel Bernabe Cruz <ma.bernabecruz@gmail.com>
2022-06-10 02:27:29 +0200

    Refactor mastermind solution

--- tech-js-procesos/mastermind/mastermind.js
+++ tech-js-procesos/mastermind/mastermind.js
@@ -1,108 +1,155 @@
 const { Console } = require("console-mpds");
 const console = new Console();
 
-const colors = "rgybmc";
-const MAX_ATTEMPTS_ALLOWED = 10;
-const SECRET_COMBINATION_LENGTH = 4;
+
+const COLORS = "rgybmc";;
+const MAX_ATTEMPTS = 10;
+const COMBINATION_LENGTH = 4;
 
 do {
-    playGame(colors, MAX_ATTEMPTS_ALLOWED, SECRET_COMBINATION_LENGTH);
-}while(console.readString(`quieres volver a jugar?:`) === 'y');
+    playGame(COLORS, MAX_ATTEMPTS, COMBINATION_LENGTH);
+} while (isResumed());
 
 
-function playGame(colors, MAX_ATTEMPTS_ALLOWED, SECRET_COMBINATION_LENGTH) {
-    let attempts = 0; 
+function playGame(colors, maxAttempts, combinationLength) {
+    let attempts = 0;
     let results = [];
-    const secretCombination = getSecretCombination(colors, SECRET_COMBINATION_LENGTH);
-    let numberOfBlacks;
+    const secretCombination = getSecretCombination(colors, combinationLength);
     do {
-        console.writeln(`${attempts} intento(s)\n ****`);
-        showResults(results);            
-        const proposalCombination = getProposalCombination(colors, SECRET_COMBINATION_LENGTH);
-        numberOfBlacks = getSuccesses(secretCombination, proposalCombination, blackCondition);
-        const numberOfWhites = getSuccesses(secretCombination, proposalCombination, whiteCondition);
-        addResult(results, numberOfBlacks, numberOfWhites, proposalCombination);
+        showBoard(attempts, results);
+        const result = playTurn(colors, secretCombination);
+        results[results.length] = result;
         attempts++;
-    }
-    while(numberOfBlacks !== SECRET_COMBINATION_LENGTH && attempts < MAX_ATTEMPTS_ALLOWED );  
-    console.writeln(numberOfBlacks === SECRET_COMBINATION_LENGTH ? "Has ganado!!! ;-)" : "Has perdido!!! :-(");
+    } while (!isEndGame(results, attempts < maxAttempts));
+    showEndGameMessage(results);
 
-    function getSecretCombination(colors, SECRET_COMBINATION_LENGTH) {
+
+
+    function getSecretCombination(colors, combinationLength) {
         let secretCombination = [];
-        for (let i = 0; i < SECRET_COMBINATION_LENGTH; i++) {
-            const randomColor = colors[parseInt(Math.random() * 7)];
-            secretCombination[i] = randomColor;
-        }
-        return secretCombination;
-    }
-
-    function showResults(results) {
-        for (const result of results) {
-            console.write(result);
-        }
-    }
-
-    function getProposalCombination(colors, SECRET_COMBINATION_LENGTH) {
-        let proposedCombination;
-        let hasInvalidColors;
         do {
-            proposedCombination = console.readString(`propon combinacion:`);
-            hasInvalidColors = false;
-            for (let i = 0; i < proposedCombination.length; i++) {
-                hasInvalidColors |= !contains(proposedCombination[i], colors);
+            const randomColor = getRandomColor(colors);
+            if (!contains(randomColor, secretCombination)) {
+                secretCombination[secretCombination.length] = randomColor;
             }
-            if (proposedCombination.length != SECRET_COMBINATION_LENGTH) {
-                console.writeln("La longitud de la combinacion es incorrecta");
-            }
-            else if (hasInvalidColors) {
-                console.writeln(`Colores inválidos, los colores son" :${colors}`);
-            }
-        } while (proposedCombination.length !== SECRET_COMBINATION_LENGTH || hasInvalidColors);
-    
-        return proposedCombination;
-    
-        function contains(character, string) {
-            let contains = false;
-            for (let i = 0; i < string.length; i++) {
-                contains |= character === string[i];              
-            }
-            return contains;
+        } while (secretCombination.length != combinationLength);
+        return secretCombination;
+
+        function getRandomColor(colors) {
+            return colors[parseInt(Math.random() * 6)];
         }
     }
 
-    function getSuccesses(secretCombination, proposalCombination, successCondition) {
-        let successes = 0;
-        for (let i = 0; i < proposalCombination.length; i++) {
-            for (let j = 0; j < secretCombination.length; j++) {
-                if (successCondition(secretCombination[i], proposalCombination[j], i, j)) {
-                    successes++;
+    function contains(character, string) {
+        for (let i = 0; i < string.length; i++) {
+            if (string[i] === character) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function showBoard(attempts, results) {
+        let msg = attempts === 0 ? '----- MASTERMIND -----\n' : "";
+        msg += `${attempts} attempt(s):`;
+        msg += '\n****';
+        console.writeln(msg);
+        for (const result of results) {
+            showResult(result);
+        }
+
+        function showResult(result) {
+            const [proposalCombination, numberOfBlacks, numberOfWhites] = result;
+            console.writeln(`${proposalCombination} --> ${numberOfBlacks} blacks and ${numberOfWhites} whites`);
+        }
+    }
+
+    function playTurn(colors, secretCombination) {
+        const proposalCombination = getProposalCombination(colors, secretCombination.length);
+        const combinations = [proposalCombination, secretCombination];
+        const numberOfBlacks = getNumberOfBlacks(combinations);
+        const numberOfWhites = getNumberOfWhites(combinations, numberOfBlacks);
+        const result = [proposalCombination, numberOfBlacks, numberOfWhites];
+        return result;
+
+
+        function getProposalCombination(colors, combinationLength) {
+            let proposalCombination;
+            let error;
+            do {
+                proposalCombination = console.readString(`Propon una combinacion:`);
+                const invalidLength = proposalCombination.length !== combinationLength;
+                if (invalidLength) {
+                    console.writeln(`- La longitud de la combinacion es incorrecta!`);
+                } else {
+                    if (hasInvalidColors(proposalCombination, colors)) {
+                        console.writeln(`- Colores invalidos, los colores son" :${colors}`);
+                    }
+                }
+                error = invalidLength || hasInvalidColors(proposalCombination, colors);
+            } while (error);
+
+            return proposalCombination;
+
+            function hasInvalidColors(proposalCombination, colors) {
+                let hasInvaliColors;
+                for (let i = 0; i < proposalCombination.length; i++) {
+                    hasInvaliColors |= !contains(proposalCombination[i], colors);
+                }
+                return hasInvaliColors;
+            }
+        }
+
+        function getNumberOfBlacks(combinations) {
+            let numberOfBlacks = 0;
+            const [proposalCombination, secretCombination] = combinations;
+            for (let i = 0; i < proposalCombination.length; i++) {
+                if (proposalCombination[i] === secretCombination[i]) {
+                    numberOfBlacks++;
                 }
             }
+            return numberOfBlacks;
         }
-        return successes;
+
+        function getNumberOfWhites(combinations, blacks) {
+            let numberOfWhites = 0;
+            const [proposalCombination, secretCombination] = combinations;
+            for (let i = 0; i < proposalCombination.length; i++) {
+                for (let j = 0; j < secretCombination.length; j++) {
+                    if (proposalCombination[i] === secretCombination[j]) {
+                        numberOfWhites++;
+                    }
+                }
+            }
+            return numberOfWhites > 0 ? numberOfWhites - blacks : numberOfWhites;
+        }
     }
 
-    function blackCondition(secretColor, proposalColor, indexSecretColor,  indexProposalColor) {
-        return secretColor === proposalColor && indexSecretColor === indexProposalColor;
+    function isEndGame(results, hasMoreAttempts) {
+        return (isWinner(results) || !hasMoreAttempts);
     }
 
-    function whiteCondition(secretColor, proposalColor, indexSecretColor,  indexProposalColor) {
-        return secretColor === proposalColor && indexSecretColor !== indexProposalColor;
+    function isWinner(results) {
+        const lastResult = results[results.length - 1];
+        const [proposalCombination, numberOfBlacks, ...rest] = lastResult;
+        return numberOfBlacks === proposalCombination.length;
     }
 
-    function addResult(results, numberOfBlacks, numberOfWhites, proposalCombination) {
-        results[results.length] = `${proposalCombination} --> ${numberOfBlacks} negras y ${numberOfWhites} blancas\n`;
+    function showEndGameMessage(results) {
+        console.writeln(isWinner(results) ? "Has ganado!!! ;-)" : "Has perdido!!! :-(");
     }
 
 }
 
-
-
-
-
-
-
-
-
-
-
+function isResumed() {
+    let error;
+    let answer;
+    do {
+        answer = console.readString(`Deseas jugar otra partida? (s/n): `);
+        error = answer !== `s` && answer !== `n`;
+        if (error) {
+            console.writeln(`Por favor, responde "s" or "n"`);
+        }
+    } while (error);
+    return answer === 's';
+}
